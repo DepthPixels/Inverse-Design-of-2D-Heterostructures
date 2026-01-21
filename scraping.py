eafdffe12512996a1f255ca9ee12f071f3fc13e1
Parav Deswal <depthclawclashedor@gmail.com>
2026-01-21 17:41:09 -0600

    get all available fields

--- scraping.py
+++ scraping.py
@@ -6,6 +6,10 @@ API_KEY = "BQX0XRiA88vGwkValqBMdLw3459tJAOA"  # Replace with your personal key
 with MPRester(API_KEY) as mpr:
     # Example query: Get materials that include Mo and S (e.g., MoS2-like compounds)
     results = mpr.materials.summary.search(material_ids=["mp-149", "mp-13", "mp-22526"])
+    
+    list_of_available_fields = mpr.materials.summary.available_fields
+    
+    print("Available Fields: ", list_of_available_fields)
 
     # Print the first few results
     for entry in results:
